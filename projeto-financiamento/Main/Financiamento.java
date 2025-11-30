7acf6ffb02ff7db694095b5e8f860186248a45b3
Ana Luiza <analu189266@gmail.com>
2025-11-30 12:23:03 -0300

    Add files via upload

---
+++ projeto-financiamento/Main/Financiamento.java
@@ -0,0 +1,52 @@
+package Main;
+
+import java.io.Serializable;
+
+public abstract class Financiamento implements Serializable {
+		// Atributos
+		protected double valorImovel;
+		protected double taxaJurosAnual;
+		protected int prazoFinanciamento;
+		
+		// Construtores
+		
+		public Financiamento(double valorImovel, int prazoFinanciamento, double taxaJurosAnual ){
+			this.valorImovel = valorImovel;
+			this.prazoFinanciamento = prazoFinanciamento;
+			this.taxaJurosAnual = taxaJurosAnual;
+		}
+			
+			public void outDadosFinais() {
+		        System.out.println("O valor do imóvel é: R$ " + getValorImovel());
+		        System.out.println("O prazo é de: " + getprazoFinanciamento() + " meses.");
+		        System.out.println("A taxa de juros anual é: " + gettaxaJurosAnual() + "%.");
+		        System.out.println("A parcela será de: R$ " + pagamentoMensal() + " por mês.");
+		        System.out.println("O valor pago ao final do financiamento é: R$ " + pagamentoTotal() + ".");
+		}
+
+		
+		// método para calcular o pagamento
+		public abstract double pagamentoMensal();
+	
+			public double pagamentoTotal() {
+				return pagamentoMensal() * prazoFinanciamento;
+			}
+			
+			
+			public double getValorImovel() {
+			    return valorImovel;
+			}
+			
+			public int getprazoFinanciamento() {
+			    return prazoFinanciamento;
+			}
+			
+			public double gettaxaJurosAnual() {
+			    return taxaJurosAnual;
+			}
+
+			public double pagMensal() {
+				
+				return 0;
+			}
+}
